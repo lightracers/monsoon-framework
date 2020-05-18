c93d2e48c4bc798c16225c66948910457ce8e25f
Lightracers Consulting <krishna@lightracers.in>
2020-05-18 22:24:15 +0530

    Added  static to return*()
    Added  static to return*()

--- src/Framework/Response.php
+++ src/Framework/Response.php
@@ -292,19 +292,19 @@ class Response
         $this->setReasonPhrase($phrase);
     }
 
-    public function return404()
+    public static function return404()
     {
         header("HTTP/1.0 404 Not Found");
         exit;
     }
 
-    public function return403()
+    public static function return403()
     {
         header("HTTP/1.1 403 Forbidden");
         exit;
     }
 
-    public function return500()
+    public static function return500()
     {
         header("HTTP/1.1 500 Internal Server Error");
         exit;
